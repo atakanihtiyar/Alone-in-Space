d3b6ece055bc16f4879e147a31d8079d8387ee44
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-07 02:24:40 +0300

    Added generic singleton code

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
+    private static Singleton<T> instance;
 
+    public static Singleton<T> Instance { get; private set; }
+
+    private void Init()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
 }
