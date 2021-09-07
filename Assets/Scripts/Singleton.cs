9a3943fe66443e70e3e63bc7faeaab2c99d67c4d
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-07 03:02:49 +0300

    Fix singleton instance properties

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -11,13 +11,13 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         Init();
     }
 
-    public static T Instance { get; private set; }
+    public static T Instance { get { return instance; } private set { instance = value; } }
 
     private void Init()
     {
-        if (instance == null)
-            instance = this as T;
-        else if (instance != this)
+        if (Instance == null)
+            Instance = this as T;
+        else if (Instance != this as T)
             Destroy(gameObject);
     }
 }
