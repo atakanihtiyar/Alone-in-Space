6f8cfa186a3d9df60d39c86afb558f27f064c1fa
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-09 23:46:18 +0300

    GameStateController updated to singleton

--- Assets/Scripts/GameStates/GameStateController.cs
+++ Assets/Scripts/GameStates/GameStateController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateController : MonoBehaviour
+public class GameStateController : Singleton<GameStateController>
 {
     private GameBaseState currentState;
 
