ac679509a729c50dff0763faf8f0dc654484e0a5
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-11 09:18:06 +0300

    Accidentally deleted Shape folder has been added and reorganized.
    accidental commit id: c93b29a5864b0fedc6dc68ddbb2292081b28141d

---
+++ Assets/Scripts/Shape/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public void ReactivateShape()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        Pattern pattern = GetComponentInParent<Pattern>();
+        pattern.deactivatedShapes.Add(this);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Shape/Spawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : UpgradedMonoBehaviour
+{
+    public GameObject[] patternPrefabs;
+    public List<Pattern> patterns;
+    public bool isCreatingContinue;
+
+    private int currentPattern = 0;
+    public float spawnYPosition;
+    public float deactivePositionY;
+
+    public static Vector2 objectPoolPosition = new Vector2(-100, -150);
+
+    private void Start()
+    {
+        StartCoroutine(CreateObjectPool());
+    }
+
+    void Update()
+    {
+        if (gameStateController.CurrentState != gameStateController.PlayState) return;
+        if (isCreatingContinue) return;
+        if (patterns[currentPattern].IsShowing()) return;
+
+        int newPattern;
+        do
+        {
+            newPattern = PickRandomPattern();
+            if (newPattern < 0)
+                return;
+        } while (newPattern == currentPattern);
+        currentPattern = newPattern;
+
+        patterns[currentPattern].Reactivate(spawnYPosition);
+    }
+
+    private IEnumerator CreateObjectPool()
+    {
+        isCreatingContinue = true;
+        for (int i = 0; i < patternPrefabs.Length; i++)
+        {
+            patterns.Add(Instantiate(patternPrefabs[i], objectPoolPosition, Quaternion.identity).GetComponent<Pattern>());
+            patterns[i].Init(deactivePositionY);
+        }
+        isCreatingContinue = false;
+        yield return null;
+    }
+
+    private int PickRandomPattern()
+    {
+        if (patterns.Count <= 1)
+        {
+            Debug.LogError("Pattern count not enough to the loop");
+            return -1;
+        }
+        Debug.Log(Pattern.TotalPossibility);
+        float randomPossibility = Random.Range(0f, Pattern.TotalPossibility);
+        float cumulativePossibility = 0f;
+
+        for (int i = 0; i < patterns.Count; i++)
+        {
+            cumulativePossibility += patterns[i].possibility;
+            if (cumulativePossibility > randomPossibility)
+                return i;
+        }
+
+        Debug.LogError("Patterns possibilities are equal to zero");
+        return -2;
+    }
+}
