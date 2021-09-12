8159786095291888cd216cbfe8bad90069b37c40
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-12 06:21:34 +0300

    Performans update - Shapes are deactivated if they are off-screen

--- Assets/Scripts/Shape/Shape.cs
+++ Assets/Scripts/Shape/Shape.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Shape : UpgradedMonoBehaviour
 {
+    private Pattern pattern;
     public GameObject particleEffect;
+
+    public void Start()
+    {
+        pattern = GetComponentInParent<Pattern>();
+    }
+
+    public void Update()
+    {
+        if (!pattern.IsShowing())
+        {
+            Deactive();
+        }
+    }
 
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return;
 
-        Pattern pattern = GetComponentInParent<Pattern>();
+        Deactive();
+        Destroy(Instantiate(particleEffect, transform.position, Quaternion.identity), 1f);
+    }
+
+    public void Deactive()
+    {
         pattern.deactivatedShapes.Add(this);
         gameObject.SetActive(false);
-        Destroy(Instantiate(particleEffect, transform.position, Quaternion.identity), 1f);
     }
 }
