29f381c022fa8494190a4536454ef98c05ecb77c
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-13 02:25:28 +0300

    XML documentation added to game state classes

--- Assets/Scripts/GameStates/GameBaseState.cs
+++ Assets/Scripts/GameStates/GameBaseState.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Base abstract class to create new state classes
+/// </summary>
 public abstract class GameBaseState
 {
+    /// <summary>
+    /// Function that should run on first pass to State 
+    /// </summary>
+    /// <param name="stateController">Controller that manages all states</param>
     public abstract void EnterState(GameStateController stateController);
+    /// <summary>
+    /// Function that should run on every update
+    /// </summary>
+    /// <param name="stateController">Controller that manages all states</param>
     public abstract void Update(GameStateController stateController);
 }
--- Assets/Scripts/GameStates/GameStateController.cs
+++ Assets/Scripts/GameStates/GameStateController.cs
@@ -3,38 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Class for managing game state changes
+/// </summary>
 public class GameStateController : Singleton<GameStateController>
 {
     private GameBaseState CurrentState { get; set; }
 
+    /// <summary>
+    /// Play state as a class
+    /// </summary>
     public readonly GamePlayState PlayState = new GamePlayState();
+    /// <summary>
+    /// Pause state as a class
+    /// </summary>
     public readonly GamePauseState PauseState = new GamePauseState();
+    /// <summary>
+    /// Game over state as a class
+    /// </summary>
     public readonly GameOverState OverState = new GameOverState();
 
+    /// <summary>
+    /// Reference to main canvas script
+    /// </summary>
     public CanvasManager canvas;
+    /// <summary>
+    /// Reference to spawner object
+    /// </summary>
     public GameObject spawner;
 
     private void Start()
     {
         TransitionToState(PauseState);
     }
-     
+
     private void Update()
     {
         CurrentState.Update(this);
     }
 
+    /// <summary>
+    /// This method is used to switch between states
+    /// </summary>
+    /// <param name="state">New state to switch</param>
     public void TransitionToState(GameBaseState state)
     {
         CurrentState = state;
         CurrentState.EnterState(this);
     }
 
+    /// <summary>
+    /// Closes the game
+    /// </summary>
     public void GameQuit()
     {
         Application.Quit();
     }
 
+    /// <summary>
+    /// Restarts the game
+    /// </summary>
     public void Restart()
     {
         TransitionToState(PlayState);
